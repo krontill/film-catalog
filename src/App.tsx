98815157d7d98bb109b21f020dc678db8f04ce62
krontill <till.work@gmail.com>
2022-01-17 14:10:41 +0200

    Add react and styled components

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {Header} from "./components/Header/Header";
+
+export const App = () => (
+  <>
+    <Header/>
+    <main>main</main>
+  </>
+);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {StyledHeader} from './styled';
+
+export const Header = () => (
+  <StyledHeader>header</StyledHeader>
+);
